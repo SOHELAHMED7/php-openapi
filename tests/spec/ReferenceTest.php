0c1fdb396a0592b4257f1834623f7452c15166e6
Sohel Ahmed Mesaniya <sam.wankaner@gmail.com>
2025-08-28 18:02:02 +0530

    Discard redundant stdout messages

--- tests/spec/ReferenceTest.php
+++ tests/spec/ReferenceTest.php
@@ -237,7 +237,7 @@ YAML
         $host = 'localhost:8787'; # create mock web server to avoid calling real GitHub URL and rate limit hit
         // $file = 'https://raw.githubusercontent.com/cebe/php-openapi/290389bbd337cf4d70ecedfd3a3d886715e19552/tests/spec/data/reference/base.yaml';
         if (stripos(PHP_OS_FAMILY, 'Windows') !== false) {
-            $cmd = 'powershell -Command "Start-Process php -ArgumentList \'-S '.$host.'\' -NoNewWindow"';
+            $cmd = 'powershell -Command "Start-Process php -ArgumentList \'-S '.$host.'\' -NoNewWindow -RedirectStandardOutput $null"';
             $res = popen($cmd, "r");
         } else {
             exec('nohup php -S '.$host.' > /dev/null 2>&1 &');
